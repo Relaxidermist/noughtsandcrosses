e51201e02414855908b350500baabe493080c341
Ingram Weeks <ingram.weeks@gmail.com>
2020-10-11 15:06:58 +0100

    Documentation added to functions

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+/*NOUGHTS AND CROSSES
+TODO: Ensure that player place entries are placed
+into gameplay tracking grid - verify this by priting
+out 2D array representing gameplay.
+
+verify that GRID SIZE for gameplay should not break
+game if broken - application should account for changing
+grid size.
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
@@ -6,13 +16,24 @@
 #define SPACEHEIGHT 6
 #define SPACEWIDTH 12
 
+
+// Scan User input from the terminal to determine next move.
 void *userinput(char *place);
+
+// Assign user moves to a grid to track gameplay.
 void updategrid(char *place, char *grid);
 
+// Main program function occurs here
 int main()
 {
-    char **rows, *columns, place[2], grid[2][2];
-    int stop, i;
+    // Local functions
+    char  **rows,     // Point to row pointers which store ascii grid/
+          *columns,   // Point to arrays which store ascii grid/
+          place[2],   // Representation of play move in 1D array - formay x,y
+          grid[2][2]; // Representation of gameplay grid used to track pieces
+
+    int   stop,       // stop flag for application
+          i;          // interfation counter for application
 
     while(stop!=1)
     {
@@ -20,7 +41,7 @@ int main()
         updategrid(place, *grid);
 
         // Allocate memory for rows in grid pointers to pointers,
-        // this is because this is a 1D array which will point to 
+        // this is because this is a 1D array which will point to
         // other 1D arrays
         rows = (char**)malloc(GRIDHEIGHT*sizeof(char*));
         // Fill array with values and print
@@ -62,30 +83,33 @@ int main()
             }
             printf("\n");
         }
-        
+
         userinput(place);
 
         free(rows);
     }
-        
+
 
     return 0;
 }
 
+
+/* This function gets the user input from the terminal and parses it
+* to determine how to move
+*/
 void *userinput(char *place)
 {
-    /* This function gets the user input from the terminal and parses it
-    * to determine how to move
-    */
-   int i;
-   char move[20], c;
+
+   int i = 0;     // Set array iterator to 0.
+   char move[20], // Array to store user input from stdin
+        c;        // char read in from stdin
 
    printf("Player select next move in format xy (i.e. 23) : ");
    scanf("%s", move);
 
-   i = 0;
-
-    do 
+   // index through user input and print until end of string
+   // populate place array
+    do
     {
         c = move[i];
 
@@ -98,15 +122,25 @@ void *userinput(char *place)
     } while(c != '\0');
 }
 
+// Update grid array with new user place to track gameplay - this function indexes
+// through place to extract the values which are used to update the grid array
+// indicating that a piece is present
 void updategrid(char *place, char *grid)
 {
-    int i, n, *m;
-    
+    int i,
+        n,
+        *m;
+
+    // Execute block if not empty string - indicated by end-of-string in place[0]
     if(place[0]!='\0')
     {
 
+        // iterate over the place array, so while i<2 as place is a 1D array with
+        // size 2.
         while(i<2)
         {
+            // Only two cases to be handled as 0<=i<2 this structure sets the indexes
+            // for the gameplay grid which should be changed.
             switch(i){
                 case 0:
                     n = place[i];
@@ -119,8 +153,24 @@ void updategrid(char *place, char *grid)
             }
             i++;
         }
+
+        // Print out indexes to verify assignment
         printf("n = %c m = %c \n", n, *m);
+
+        // set gameplay grid at indexes to 1 to indicate piece is present.
         grid[n][m] = 1;
+
+        // Print grid value at indexes to verify that grid value is correct
         printf("User Input!: %d\n", grid[n][m]);
+
+        // Iterate over rows and columns of gameplay grid to show all values
+        // of index in the grid.
+        for(n = 0; n<=2; n++)
+        {
+            for(*m = 0; *m<=2; *m++)
+            {
+                printf("grid[%d][%d] = %d\n", n, *m, grid[n][m]);
+            }
+        }
     }
 }
